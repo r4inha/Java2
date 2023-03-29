ff1ca309bbcd5fa3438a92daeed49019c6d9c8d0
r4inha <kadu_300live.com>
2023-03-29 11:51:31 -0300

    Criação de controller e repository

--- conta/src/controller/ContaController.java
+++ conta/src/controller/ContaController.java
@@ -9,37 +9,36 @@ public class ContaController implements ContaRepository {
 		// TODO Auto-generated method stub
 		
 	}
-
 	public void listarTodas() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@Override
 	public void procurarPorNumero(int numero) {
 		// TODO Auto-generated method stub
 		
 	}
-
+    @Override
 	public void atualizar(Conta conta) {
 		// TODO Auto-generated method stub
 		
 	}
-
+    @Override
 	public void deletar(int numero) {
 		// TODO Auto-generated method stub
 		
 	}
-
+    @Override
 	public void sacar(int numero, float valor) {
 		// TODO Auto-generated method stub
 		
 	}
-
+    @Override
 	public void depositar(int numero, float valor) {
 		// TODO Auto-generated method stub
 		
 	}
-
+    @Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
 		// TODO Auto-generated method stub
 		
