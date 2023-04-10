6cd3b969146b611934e5435f406a626b48f8e474
Bianca Francis <kadu_300@live.com>
2023-04-10 07:00:35 -0300

    Update Conta.java

--- conta/src/model/Conta.java
+++ conta/src/model/Conta.java
@@ -1,13 +1,12 @@
-package model;
+package conta_bancaria.model;
 
-public class Conta {
-
+public abstract class Conta {
+	
 	private int numero;
 	private int agencia;
 	private int tipo;
 	private String titular;
 	private float saldo;
-	
 	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
 		this.numero = numero;
 		this.agencia = agencia;
@@ -15,47 +14,37 @@ public class Conta {
 		this.titular = titular;
 		this.saldo = saldo;
 	}
-
 	public int getNumero() {
 		return numero;
 	}
-
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-
 	public int getAgencia() {
 		return agencia;
 	}
-
-	public void setAgencia(int agencia) {
-		this.agencia = agencia;
-	}
-
 	public int getTipo() {
 		return tipo;
 	}
-
-	public void setTipo(int tipo) {
-		this.tipo = tipo;
-	}
-
 	public String getTitular() {
 		return titular;
 	}
-
-	public void setTitular(String titular) {
-		this.titular = titular;
-	}
-
 	public float getSaldo() {
 		return saldo;
 	}
-
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
 	public void setSaldo(float saldo) {
 		this.saldo = saldo;
 	}
-
+	
 	public boolean sacar(float valor) {
 		
 		if (this.getSaldo() < valor) {
@@ -65,33 +54,26 @@ public class Conta {
 		
 		this.setSaldo(this.getSaldo() - valor);
 		return true;
-		
 	}
 	
-	public void depositar(float valor) {
+	public void depositar (float valor) {
 		this.setSaldo(this.getSaldo() + valor);
 	}
 	
 	public void visualizar() {
-		
 		String tipo = "";
-		
 		switch(this.tipo) {
-			case 1 -> tipo = "Conta Corrente";
-			case 2 -> tipo = "Conta poupança";
+		case 1 -> tipo = "conta corrente";
+		case 2 -> tipo = "conta poupança";
 		}
-		
-		System.out.println("**************************************");
-		System.out.println("                                      ");
-		System.out.println("           DADOS DA CONTA             ");
-		System.out.println("                                      ");
-		System.out.println("**************************************");
-		System.out.println("Numero da conta: " + this.numero);
-		System.out.println("Numero da agência: " + this.agencia);
+		System.out.println("**********************************************");
+		System.out.println("                  DADOS DA CONTA              ");
+		System.out.println("**********************************************");
+		System.out.println("Número da conta: " + this.numero);
+		System.out.println("Número da agência: " + this.agencia);
 		System.out.println("Tipo da conta: " + tipo);
 		System.out.println("Nome do titular da conta: " + this.titular);
 		System.out.println("Saldo da conta: " + this.saldo);
-		
 	}
 
 }
