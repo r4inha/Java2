3f4c6b933bf776835c124b59705385317e5e0b23
Bianca Francis <kadu_300@live.com>
2023-04-10 07:02:26 -0300

    Update ContaController.java

--- conta/src/controller/ContaController.java
+++ conta/src/controller/ContaController.java
@@ -1,58 +1,134 @@
-package controller;
+package conta_bancaria.controller;
 
-import model.Conta;
-import repository.ContaRepository;
+import java.util.ArrayList;
 
-public class ContaController implements ContaRepository {
-	public void cadastrar(Conta conta) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void listarTodas() {
-		// TODO Auto-generated method stub
-		
-	}
+import conta_bancaria.model.Conta;
+import Conta.Repository.ContaRepository;
+
+public class Controller implements ContaRepository {
+	
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
+	int numero = 0;
+	
+
 	@Override
 	public void procurarPorNumero(int numero) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
 		
-	}
-    @Override
-	public void atualizar(Conta conta) {
-		// TODO Auto-generated method stub
-		
-	}
-    @Override
-	public void deletar(int numero) {
-		// TODO Auto-generated method stub
-		
-	}
-    @Override
-	public void sacar(int numero, float valor) {
-		// TODO Auto-generated method stub
-		
-	}
-    @Override
-	public void depositar(int numero, float valor) {
-		// TODO Auto-generated method stub
-		
-	}
-    @Override
-	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
+		if(conta != null)
+			conta.visualizar();
+		else 
+			System.out.println("A conta não foi encontrada!");
 		
 	}
 
 	@Override
 	public void listartodas() {
-		// TODO Auto-generated method stub
+		for (var conta : listaContas) {
+			conta.visualizar();
+		}
 		
 	}
 
 	@Override
 	public void Cadastrar(Conta conta) {
-		// TODO Auto-generated method stub
+		listaContas.add(conta);
+		System.out.println("A conta número: " + conta.getNumero() + " foi criada com sucesso!");
 		
 	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		
+		var buscaConta = buscarNaCollection(conta.getNumero());
+
+		if (buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("Os dados da Conta foram atualizados com sucesso!");
+		}else
+			System.out.println("A conta não foi encontrada!");
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null) {
+			if(listaContas.remove(conta) == true)
+				System.out.println("A Conta foi excluída!");
+		}else
+			System.out.println("A conta não foi encontrada!");
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null) {
+			if(conta.sacar(valor) == true)
+				System.out.println("O Saque foi efetuado com sucesso!");
+		}else
+			System.out.println("A conta não foi encontrada!");
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null) {
+			conta.depositar(valor);
+				System.out.println("O Depósito foi efetuado com sucesso!");
+		}else
+			System.out.println("A conta não foi encontrada!");
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		
+		var contaOrigem = buscarNaCollection(numeroOrigem);
+		var contaDestino = buscarNaCollection(numeroDestino);
+		
+		if (contaOrigem != null && contaDestino != null) {
+			if(contaOrigem.sacar(valor) == true) {
+				contaDestino.depositar(valor);
+				System.out.println("A Transferência foi efetuada com sucesso!");
+			}
+		}else
+			System.out.println("A conta não foi encontrada!");
+		
+	}
+	
+	
+	public int gerarNumero() {
+		return ++ numero;
+	}
+	
+	public Conta buscarNaCollection(int numero) {
+		for(var conta : listaContas) {
+			if(conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		
+		return null;
+	}
+	
+	public int retornaTipo(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+				return conta.getTipo();
+			}
+		}
+
+		return 0;
+	}
+	
 
 }
